617386b8813688e075225b3d686a3fdb344a663d
Artur-Petrov <petrov.artur02@gmail.com>
2025-12-25 16:05:09 +0200

    finished styling for pc screens

---
+++ FirstTask/script/main.js
@@ -0,0 +1,11 @@
+const list = document.getElementById('usersOutputList');
+const emptyMessage = document.getElementById('emptyMessage');
+
+function updateEmptyState() {
+    if (list.children.length === 0) {
+        emptyMessage.style.display = 'block';
+    } else {
+        emptyMessage.style.display = 'none';
+    }
+}
+updateEmptyState();
