b37ff14042f1c80d51cd4979b3ebfde503bcfff3
Artur-Petrov <petrov.artur02@gmail.com>
2026-01-09 01:32:52 +0200

    post-details task

---
+++ SecondTask/scripts/post-details.js
@@ -0,0 +1,32 @@
+// getting posts id
+const postId = new URL(location.href).searchParams.get('id');
+
+let postContainer = document.getElementById('post-container');
+let commentsContainer = document.getElementById('comments-container');
+
+//output for information about post
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+.then(response => response.json())
+.then(post => {
+    postContainer.innerHTML = `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    `;
+})
+// output for comments
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+.then(response => response.json())
+.then(comments => {
+    for (const comment of comments) {
+        let commentDiv = document.createElement('div');
+        commentDiv.classList.add('comment-card');
+
+        commentDiv.innerHTML = `
+        <h5>Id${comment.id}</h5>
+        <p>${comment.email}</p>
+        <p>${comment.body}</p>
+        `;
+
+        commentsContainer.appendChild(commentDiv);
+    }
+})
